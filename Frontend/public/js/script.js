7431965ba73aa877768c289e1c0246ef0d448e6d
Itsarawut Churat - 6609612335 <itsarawut.chu@dome.tu.ac.th>
2024-11-02 14:02:03 +0700

    edit validation & fetch API

--- Frontend/public/js/script.js
+++ Frontend/public/js/script.js
@@ -1,29 +1,72 @@
+function submitLogin(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (!validateUsername() || !validatePassword()) {
+        return;
+    }
+
+    url = 'https://restapi.tu.ac.th/api/v1/auth/Ad/verify';
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Application-Key': '{TOKEN}'//ใส่ Token ใช้งาน API
+       },
+       body: JSON.stringify({"UserName": username, "PassWord": password})
+    })
+    .then(response =>{
+        if(!response.ok){
+            throw new Error('Login failed!');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.message);
+    })
+    .catch(error => {
+        console.error('Error:', error)
+        document.getElementById('error-message').innerText = 'Login failed Password Invalid';
+    })
+}
+
+//validate username
 function validateUsername() {
     const username = document.getElementById('username').value;
     const errorMessage = document.getElementById('error-message');
     errorMessage.style.color = 'red';
 
-    if (username = null || username === '') {
-        errorMessager.innerText = 'กรุณากรอกชื่อผู้ใช้';
-        return;
+    if (!username.length > 0) {
+        errorMessage.innerText = 'กรุณากรอกชื่อผู้ใช้';
+        return false;
     }
     if (username.length < 10) {
         errorMessage.innerText = 'ชื่อผู้ใช้ต้องมีความยาว 10 หลัก';
-        return;
+        return false;
+    }
+    else{
+        errorMessage.innerText = '';
+        return true;
     }
 }
 
+//validate password
 function validatePassword() {
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
     errorMessage.style.color = 'red';
 
-    if (password = null || password === '') {
+    if (!password) {
         errorMessage.innerText = 'กรุณากรอกรหัสผ่าน';
-        return
+        return false;
     }
     if (password.length < 3) {
-        errorMessager.innerText = 'รหัสผ่านต้องมีความยาวมากกว่า 3 ตัว';
-        return;
+        errorMessage.innerText = 'รหัสผ่านต้องมีความยาวมากกว่า 3 ตัว';
+        return fasle;
+    }
+    else{
+        errorMessage.innerText = '';
+        return true;
     }
 }
