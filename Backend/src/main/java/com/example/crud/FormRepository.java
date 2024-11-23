6deb5df0f7815e8596030b316d517034ddeeafc0
ktp.mtl <kongza.cgevent@gmail.com>
2024-11-24 01:23:31 +0700

    delete olds

--- Backend/src/main/java/com/example/crud/FormRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import org.springframework.stereotype.Service;
-public interface FormRepository extends JpaRepository<Form, Long> {
-    // You can add custom query methods if needed
-    List<Form> findByStudentId(String studentId);
-}
